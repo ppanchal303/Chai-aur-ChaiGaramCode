ee539c8f1032587f00848bbc06ce4470cecdb544
Pranav Panchal <pranavpanchal303@gmail.com>
2024-11-17 15:39:55 -0500

    trial commit

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,8 @@
+const userName = 'Chai Garam' // most comman to use during a variable declaration
+let userId = 456321 // to be used only if the variable value might change
+var userEmail = 'chai@example.com' // not to be used
+city = 'Toronto' // not to be used
+
+/*
+  The var variable declaration is not used as it does not follow block scope and functional scope
+*/
