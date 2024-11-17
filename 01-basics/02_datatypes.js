0b28c573120a675f353e3d1e43eef08492e1030d
Pranav Panchal <pranavpanchal303@gmail.com>
2024-11-17 16:28:33 -0500

    add datatype file

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,31 @@
+// "use strict"; // treat all JS code as the newer version of javascript
+
+//alert(3+3)
+/*
+  does not work in the node environment (in VS code, we use node.js to execute the JS files), this works in the browser env
+*/
+
+let userName = 'Brunno' // String
+let age = 5 // Number
+let isLoggedIn = false // Boolean
+
+/*
+JavaScript has 8 Datatypes
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+
+The Object Datatype
+The object data type can contain both built-in objects, and user defined objects:
+
+Built-in object types can be:
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more...
+*/
+
+// to know the datatype of a variable use can use typeof
+console.log(typeof userName)
